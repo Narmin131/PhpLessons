773ca50295eb09a2e044ecb387480b03cc150584
Narmin <mnermin064@gmail.com>
2022-06-13 18:46:24 +0400

    new commit

---
+++ dataTypes/index.php
@@ -0,0 +1,60 @@
+<?php
+
+// $name = "Narmin";
+// echo $name;
+
+// Data Types
+// String
+// $stringInfo = "Hello <br>";
+// echo $stringInfo;
+// echo var_dump($stringInfo)."<br>";
+
+// Integer
+// $intInfo = 5;
+// $intInfo1 = 10;
+// echo $intInfo + $intInfo1."<br>";
+// echo is_int(var_dump($stringInfo));
+
+
+// Float (floating point numbers - also called double)
+// $floatInfo = 5.1;
+// $floatInfo1 = 10.39;
+// echo $floatInfo + $floatInfo1;
+
+
+// Boolean
+// true = 1, false = 0 or nothing
+// if (true) {
+//     echo true;
+// }else{
+//     echo false;
+// }
+
+// Array
+// $infoArr = [1,2,3,'Hello',true];
+// $infoArr = array(1,2,3,'Hello',true);
+// echo var_dump($infoArr);
+// echo $infoArr[3];
+
+// Object
+// class Car{
+//     public function carInfo($a){
+//         return $a;
+//     } 
+// }
+// $myCar = new Car();
+// echo $myCar->carInfo("New Hello");
+
+
+// NULL
+// $nullInfo = null;
+// echo var_dump($nullInfo);
+// Resource
+// PHP Resource
+// The special resource type is not an actual data type. It is the storing of a reference to functions and resources external to PHP.
+
+// A common example of using the resource data type is a database call.
+
+// We will not talk about the resource type here, since it is an advanced topic.
+
+?>
---
+++ operators/index.php
@@ -0,0 +1,112 @@
+<?php
+// Arithmetic operators
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// /	Division
+// %	Modulus
+// **	Exponentiation
+// $countOne = 5;
+// $countTwo = 10;
+// $resultCount = $countOne ** $countTwo;
+// echo $resultCount;
+// ======================================================
+
+// Assignment operators
+// +=	Addition
+// -=	Subtraction
+// *=	Multiplication
+// /=	Division
+// %=	Modulus
+// ======================================================
+
+// Comparison operators
+// ==	Equal
+// ===	Identical
+// !=	Not equal
+// <>	Not equal
+// !==	Not identical
+// >	Greater than
+// <	Less than
+// >=	Greater than or equal to
+// <=	Less than or equal to
+// <=>	Spaceship
+// $x = 5;  
+// $y = 10;
+
+// echo ($x <=> $y); // returns -1 because $x is less than $y
+// echo "<br>";
+
+// $x = 10;  
+// $y = 10;
+
+// echo ($x <=> $y); // returns 0 because values are equal
+// echo "<br>";
+
+// $x = 15;  
+// $y = 10;
+
+// echo ($x <=> $y); // returns +1 because $x is greater than $y
+// ======================================================
+
+
+// Increment/Decrement operators
+// ++$x	Pre-increment
+// $x++	Post-increment
+// --$x	Pre-decrement
+// $x--	Post-decrement
+// ======================================================
+
+// Logical operators
+// and	And
+// or	Or
+// xor	Xor
+// &&	And
+// ||	Or
+// !	Not
+// if (true or false) {
+//     echo 'true';
+// }else{
+//     echo 'false';
+// }
+
+// $x = 100;  
+// $y = 20;
+
+// if ($x == 100 xor $y == 50) {
+//     echo 'true';
+// }else{
+//     echo 'false';
+// }
+// ======================================================
+
+// String operators
+// .	Concatenation
+// .=	Concatenation assignment
+// $name = "Jhon";
+// $name = "Morgan";
+// echo $name." ".$surname;
+// ======================================================
+
+// Array operators
+// Conditional assignment operators
+// $x = array("a" => "red", "b" => "green");  
+// $y = array("c" => "blue", "d" => "yellow"); 
+// echo var_dump($x<>$y) ;
+// echo $x['a'];
+// ======================================================
+
+
+
+// PHP Conditional Assignment Operators
+
+// if empty($user) = TRUE, set $status = "anonymous"
+//   echo $status = (empty($user)) ? "anonymous" : "logged in";
+//   echo("<br>");
+// ?:	Ternary
+//   $user = "John Doe";
+  // if empty($user) = FALSE, set $status = "logged in"
+//   echo $status = (empty($user)) ? "anonymous" : "logged in";
+
+// Copyright -> https://www.w3schools.com/php/php_operators.asp
+?>
